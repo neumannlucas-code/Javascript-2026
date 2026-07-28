a1d56a21418a9cd054bdf5b683fb4676614fc28b
Zazas <neumann.lucas@escola.pr.gov.br>
2026-07-28 11:21:16 -0300

    Add files via upload

---
+++ Javascript-1ds-2026/aula01.js
@@ -0,0 +1,10 @@
+// CRIANDO VARIÁVEIS PARA O SISTEMA
+
+let nome = "Lucas";
+var idade = 33;
+const altura = 1.79;
+
+console.log(nome);
+console.log(idade);
+console.log(altura);
+
---
+++ Javascript-1ds-2026/aula02.js
@@ -0,0 +1,13 @@
+// CONDICIONAIS
+
+let nota1 = 6;
+let nota2 = 10;
+let nota3 = 5;
+
+mediaFinal = nota1 + nota2 + nota3;
+
+if mediaDinal > 6) {
+   console.log("Aluno aprovado!");
+}else{
+    console.log("Aluno reprovado");
+}   
